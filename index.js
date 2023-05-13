c1ff571ad15bd398c327acf775c7be966b6b8ac7
JayaPrakashSriram <sriram.fsd@gmail.com>
2023-05-13 10:01:48 +0530

    Day17

---
+++ index.js
@@ -0,0 +1,49 @@
+let container = document.createElement("div")
+container.classList.add("conatiner")
+
+let span = document.getElementById("span")
+span.innerHTML = 10
+
+
+document.body.append(container)
+let count = 10;
+setTimeout(()=>{
+  count = count-1
+  span.innerHTML = count
+  setTimeout(()=>{
+    count = count-1
+    span.innerHTML = count
+    setTimeout(()=>{
+      count = count-1
+      span.innerHTML = count
+      setTimeout(()=>{
+        count = count-1
+        span.innerHTML = count
+        setTimeout(()=>{
+          count = count-1
+          span.innerHTML = count
+          setTimeout(()=>{
+            count = count-1
+            span.innerHTML = count
+            setTimeout(()=>{
+              count = count-1
+              span.innerHTML = count
+              setTimeout(()=>{
+                count = count-1
+                span.innerHTML = count
+                setTimeout(()=>{
+                  count = count-1
+                  span.innerHTML = count
+                  setTimeout(()=>{
+                    count = count-1
+                    span.innerHTML = "Happy Independence Day✨"
+                  },1000)
+                },1000)
+              },1000)
+            },1000)
+          },1000)
+        },1000)
+      },1000)
+    },1000)
+  },1000)
+},1000)
